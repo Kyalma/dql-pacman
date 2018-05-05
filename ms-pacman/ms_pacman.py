8f45c7ce757d6053d68075b6647f5cef555e9ae3
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-05-04 20:15:58 -0700

    code refactor

--- ms-pacman/ms_pacman.py
+++ ms-pacman/ms_pacman.py
@@ -1,5 +1,7 @@
 ## Léo Poulin
+## 017465433
 
+import os
 import time
 
 from keras.models import Sequential      # One layer after the other
@@ -11,22 +13,19 @@ import random     # For sampling batches from the observations
 import numpy as np
 import gym                                # To train our network
 
-GAME_OBSERVETIME = 5
-BATCH_SIZE = 32
-
-OBSERVETIME = 2000                          # Number of timesteps we will be acting on the game and observing results
-RANDOM_THRESHOLD = 0.6                              # Probability of doing a random move
-GAMMA = 0.9                                # Discounted future reward. How much we care about steps further in time
-MINIBATCH_SIZE = 50                               # Learning minibatch size
 
 class MsPacman():
     def __init__(self):
         self.env = gym.make('MsPacman-v0')
+        self.weights_file = './ms-pacman-w.h5'
         self.memory = deque()
+        self.observation_loops = 5
+        self.batch_size = 32
         self.learning_rate = 0.01
         self.exploration_rate = 0.7
         self.explotation_rate_min = 0.1
         self.exploration_decay = 0.9
+        self.discount_rate = 0.9
         ## Creating the model
         self.model = Sequential()
         self.model.add(Conv2D(16, (3, 3),                       # Conv 2D
@@ -51,6 +50,13 @@ class MsPacman():
         # self.model.add(Activation('softmax'))                 #  * softmax
         self.model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 
+    def load(self):
+        if os.path.exists(self.weights_file):
+            self.model.load_weights(self.weights_file)
+
+    def save(self):
+        self.model.save_weights(self.weights_file)
+
     def summary(self) -> None:
         self.model.summary()
 
@@ -61,34 +67,33 @@ class MsPacman():
         obs = self.env.reset()
         state = np.expand_dims(obs, axis=0)
         print("Observing Game 1")
-        while nb_games_done < GAME_OBSERVETIME:
+        while nb_games_done < self.observation_loops:
             self.env.render()
             if np.random.rand() <= self.exploration_rate:
                 action = np.random.randint(0, 4, size=1)[0]
             else:
-                q_values = self.model.predict(state)                     # Q-values predictions
-                action = np.argmax(q_values)                        # Move with highest Q-value is the chosen one
-            obs_new, reward, done, _ = self.env.step(action)          # See state of the game, reward... after performing the action
+                q_values = self.model.predict(state)                        # Q-values predictions
+                action = np.argmax(q_values)                                # Choose the move with the highest reward from the prediction
+            obs_new, reward, done, _ = self.env.step(action)
             game_reward += reward
             state_new = np.expand_dims(obs_new, axis=0)
-            self.memory.append((state, action, reward, state_new, done))          # 'Remember' action and consequence
-            # state = np.append(state_new, state, axis=0)
-            state = state_new                                       # Update state
+            self.memory.append((state, action, reward, state_new, done))    # 'Remember' action and consequence
+            state = state_new                                               # Update state
             if done:
                 print("Total reward for this game: {}".format(game_reward))
                 nb_games_done += 1
                 game_reward = 0
-                obs = self.env.reset()                                   # Game begins
-                state = np.expand_dims(obs, axis=0)                 # (Formatting issues) Making the observation the first element of a batch of inputs 
+                obs = self.env.reset()                                      # Restart the game
+                state = np.expand_dims(obs, axis=0)
                 print("Observing Game {}".format(nb_games_done + 1))
         print('Observation done')
 
     def learn_from_replay(self) -> None:
-        replay_batch = random.sample(self.memory, BATCH_SIZE)
+        replay_batch = random.sample(self.memory, self.batch_size)
         for state, action, reward, next_state, done in replay_batch:
             if not done:
                 optimal_future_value = np.amax(self.model.predict(next_state)[0])
-                target = reward + GAMMA * optimal_future_value
+                target = reward + self.discount_rate * optimal_future_value
             else:
                 target = reward
             target_f = self.model.predict(state)
@@ -111,44 +116,18 @@ class MsPacman():
             tot_reward += reward
         print('Game ended! Total reward: {}'.format(tot_reward))
 
-
-# def learn(model, register, state):
-#     # SECOND STEP: Learning from the observations (Experience replay)
-#     minibatch = random.sample(register, MINIBATCH_SIZE)         # Sample some moves
-#     inputs_shape = (MINIBATCH_SIZE,) + state.shape[1:]
-#     inputs = np.zeros(inputs_shape)
-#     targets = np.zeros((MINIBATCH_SIZE, ENV.action_space.n))
-#     for i in range(0, MINIBATCH_SIZE):
-#         state = minibatch[i][0]
-#         action = minibatch[i][1]
-#         reward = minibatch[i][2]
-#         state_new = minibatch[i][3]
-#         done = minibatch[i][4]        
-#     # Build Bellman equation for the Q function
-#         inputs[i:i+1] = np.expand_dims(state, axis=0)
-#         targets[i] = model.predict(state)
-#         Q_sa = model.predict(state_new)
-        
-#         if done:
-#             targets[i, action] = reward
-#         else:
-#             targets[i, action] = reward + GAMMA * np.max(Q_sa)
-#     # Train network to output the Q function
-#         model.train_on_batch(inputs, targets)
-#     print('Learning Finished')
-
 def main():
     agent = MsPacman()
     agent.summary()
-    # agent.model.load_weights('mspacman-weights.h5')
+    agent.load()
     for iteration in range(10):
         print("Iteration {}".format(iteration))
         agent.observe()
         agent.learn_from_replay()
         agent.play()
+        agent.save()
         agent.memory.clear()
-        agent.model.save_weights('mspacman-weights.h5')
+        
 
 if __name__ == "__main__":
-    # test_play()
     main()
